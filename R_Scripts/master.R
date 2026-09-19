efc199f0890073cbbd381ce658ba2db3b3893267
Jenny <qujenny1@gmail.com>
2026-09-19 17:42:27 -0400

    your message here

--- R_Scripts/master.R
+++ R_Scripts/master.R
@@ -10,4 +10,12 @@ senate_polls <- read_excel(
 
 tariffed_imports <- read_excel(
   path = here("data/tariffedImportsByState.xlsx")
-)
+) %>%
+  rename(state = State) %>%
+  mutate(state = str_trim(state))
+
+senate_polls <- senate_polls %>%
+  mutate(state = str_trim(state))
+
+merged <- senate_polls %>%
+  left_join(tariffed_imports, by = "state")
